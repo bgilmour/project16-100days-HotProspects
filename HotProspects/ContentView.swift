1470fe7674d9df09d650a0c40753b7d7e9b5d689
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-09 22:52:18 +0100

    Scheduling local notifications

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        ContextMenuTestView()
+        NotificationTestView()
+    }
+}
+
+struct NotificationTestView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
     }
 }
 
