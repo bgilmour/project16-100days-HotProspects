bf2396eda07f8c1fa8d379d7e17b69d9a4f8262f
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-09 16:55:32 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Bruce Gilmour on 2021-08-09.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
