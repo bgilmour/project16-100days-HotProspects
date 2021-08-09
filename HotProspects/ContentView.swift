61d6828e33470e2b5fa2d8b40903b1386b0a0023
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-09 17:55:44 +0100

    Intro to EnvironmentObject and TabView

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,8 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabsTest2View()
+    }
+}
+
+struct TabsTest1View: View {
+    var body: some View {
+        TabView {
+            Text("Tab 1")
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+        }
+    }
+}
+
+struct TabsTest2View: View {
+    @State private var selectedTab = 0
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = 1
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+                .tag(0)
+
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+                .tag(1)
+        }
+    }
+}
+
+class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct EnvObjectTest1View: View {
+    var user = User()
+
+    var body: some View {
+        VStack {
+            EditView().environmentObject(user)
+            DisplayView().environmentObject(user)
+        }
+    }
+}
+
+struct EnvObjectTest2View: View {
+    var user = User()
+
+    var body: some View {
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        .environmentObject(user)
     }
 }
 
